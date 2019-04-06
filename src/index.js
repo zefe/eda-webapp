4499f146d9085c4d873d390c8bfaa240fdf227e2
zefe <zefetorres@icloud.com>
2019-04-06 00:08:06 -0600

    add webpack, babel and react configuration

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const App = () => (
+    <div>
+        <h1>Mira mama sin manos</h1>
+    </div>
+)
+
+ReactDOM.render(<App/>, document.getElementById('root'));
