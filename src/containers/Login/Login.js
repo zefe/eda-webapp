f46fe5325f6d085a57c7122ff14b1a8097f46579
zefe <zefetorres@icloud.com>
2019-04-07 19:29:19 -0500

    add fix text in buttons

--- src/containers/Login/Login.js
+++ src/containers/Login/Login.js
@@ -16,7 +16,23 @@ class Login extends Component {
                             {/* <h1>Exploratory Data Analisis</h1> */}
                             <h1>Create, collaborate and define the information of your projects for exploratory data analysis.</h1>
                         </div>
-                        <div className="login-form-container">
+                        <div className="form">
+                            <h1>Sign in to eda</h1>
+                            <form action="">
+                                <div className="form-group">
+                                    <label htmlFor="">EMAIL</label>
+                                    <input type="text" name="" id="" placeholder="juanita@example.com" />
+                                </div>
+                                <div className="form-group">
+                                    <label htmlFor="">PASSWORD</label>
+                                    <input type="text" name="" id="" placeholder="Enter your password" />
+                                </div>
+                            </form>
+                            <button className="btn btn-primary">Sign in</button>
+                        </div>
+
+
+                        {/* <div className="login-form-container">
                             <h1>Sign in to eda</h1>
                             <div className="form">
                                 <form action="">
@@ -30,9 +46,8 @@ class Login extends Component {
                                     </div>
                                 </form>
                                 <button className="btn btn-primary">Sign Up</button>
-                                {/* <p>Al registrarte, aceptas nuestras Condiciones, la Política de datos y la Política de cookies.</p> */}
                             </div>
-                        </div>
+                        </div> */}
 
                     </div>
                 </div>
