a522c20d86162a4ba3e254d23eddcea2d96a584b
zefe <zefetorres@icloud.com>
2019-04-19 20:42:00 -0500

    add user form component

---
+++ src/components/UserForm/UserForm.js
@@ -0,0 +1,57 @@
+import React from 'react';
+
+function UserForm(props) {
+    return (
+        <div>
+            <h1>Add a new user to the team</h1>
+            <form action="" onSubmit={props.handleSubmit}>
+                <div className="form-group">
+                    <label htmlFor="">FIRST NAME</label>
+                    <input
+                        type="text"
+                        name="firstName"
+                        id="firstName"
+                        placeholder="Juanita"
+                        onChange={props.handleChange}
+                        value={props.formValues.firsName}
+                    />
+                </div>
+                <div className="form-group">
+                    <label htmlFor="">LAST NAME</label>
+                    <input
+                        type="text"
+                        name="lastName"
+                        id="lastName"
+                        placeholder="Flores"
+                        onChange={props.handleChange}
+                        value={props.formValues.lastName}
+                    />
+                </div>
+                <div className="form-group">
+                    <label htmlFor="">EMAIL</label>
+                    <input type="text"
+                        name="email"
+                        id="email"
+                        placeholder="juanita@example.com"
+                        onChange={props.handleChange}
+                        value={props.formValues.email} />
+                </div>
+                <div className="form-group">
+                    <label htmlFor="">PASSWORD</label>
+                    <input
+                        type="password"
+                        name="password"
+                        id="password"
+                        placeholder="Enter your password"
+                        onChange={props.handleChange}
+                        value={props.formValues.password} />
+                </div>
+                <div className="form-button" >
+                    <button className="btn btn-primary" >Create</button>
+                </div>
+            </form>
+        </div>
+    )
+}
+
+export default UserForm;
