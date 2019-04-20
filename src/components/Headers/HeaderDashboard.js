aa486d485e04f0930a68af8281730e1b4d03c0aa
zefe <zefetorres@icloud.com>
2019-04-19 20:43:20 -0500

    add header component to dashboard

---
+++ src/components/Headers/HeaderDashboard.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import logo from '../../images/logo.png';
+import person from '../../images/person.jpg';
+import './HeaderDashboard.css';
+
+function Header(props) {
+    return (
+        <div className="container-header">
+            <div className="header">
+                <div className="header-logo">
+                    <img src={logo} alt="logo" />
+                    <h1>EDA</h1>
+                </div>
+                <div className="header-avatar">
+                    <img src={person} alt="" width={40} />
+                    <div className="description">
+                        <span>Juanita Flores</span>
+                        <small>Team de juanita</small>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Header;
+
