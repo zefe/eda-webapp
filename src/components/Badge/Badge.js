dcd9091b64d294519e49edd6c99ab6162ede9ac7
zefe <zefetorres@icloud.com>
2019-04-21 00:11:00 -0500

    fix Badge component

--- src/components/Badge/Badge.js
+++ src/components/Badge/Badge.js
@@ -1,26 +1,24 @@
-import React, { Component } from 'react';
+import React from 'react';
 
 import './Badge.css';
 import Gravatar from '../Gravatar/Gravatar';
 
-class Badge extends Component {
-    render() {
-        return (
-            <div className="badge">
+function Badge(props) {
+    return (
+        <div className="badge">
 
-                <div className="badge-section-name">
-                    <Gravatar className="badge-avatar" email={this.props.email} />
-                    <h1>
-                        {this.props.firstName} <br /> {this.props.lastName}
-                    </h1>
-                </div>
-
-                <div className="badge-section-info">
-                    <h3>{this.props.email}</h3>
-                </div>
+            <div className="badge-section-name">
+                <Gravatar className="badge-avatar" email={props.email} />
+                <h1>
+                    {props.firstName} <br /> {props.lastName}
+                </h1>
             </div>
-        );
-    }
+
+            <div className="badge-section-info">
+                <h3>{props.email}</h3>
+            </div>
+        </div>
+    );
 }
 
 export default Badge;
