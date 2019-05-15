31250095e850f44bf5b95dea5e75804a9efe434f
zefe <zefetorres@icloud.com>
2019-05-15 14:27:17 -0500

    update home route

--- src/App.js
+++ src/App.js
@@ -22,13 +22,13 @@ function App() {
     return (
         <BrowserRouter>
             <Switch>
-                <Route exact path="/sss" component={Login} />
+                <Route exact path="/" component={Login} />
                 <Route exact path="/signup" component={SignUp} />
                 <Layout>
                     <Header />
                     <Navigation />
                     <Route exact path="/graphs" component={Graphs} />
-                    <Route exact path="/" component={Projects} />
+                    <Route exact path="/projects" component={Projects} />
                     <Route exact path="/team" component={TeamUsers} />
                     <Route exact path="/user/new" component={NewUser} />
                     <Route exact path="/users/:userId/edit" component={EditUser} />
